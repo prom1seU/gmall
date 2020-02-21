5d6ea298bcb191671f4e7a1cede4d7d9aff78b27
Wei.Xiong <87752465@qq.com>
2020-02-21 11:31:25 +0800

    normal commit

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,16 @@
 package com.atguigu.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1.配置整合dubbo
+ * 2.配置整合MybatisPlus
+ */
+@EnableDubbo
+@MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,33 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    ProductService productService;
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+
+//        Product p = productService.getById(1);
+//        System.out.println(p.getName());
+
+        //测试增删改在主库，查询在从库
+        Brand brand = new Brand();
+        brand.setName("哈哈哈");
+        brandService.save(brand);
+
+        System.out.println("保存成功");
     }
 
 }
